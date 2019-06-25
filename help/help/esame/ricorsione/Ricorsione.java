0a114bf0273205399fedf8205a04abf62c875562
Angelica <Angelica@DESKTOP-1QUPAUU>
2019-06-25 16:05:09 +0200

    .

--- help/help/esame/ricorsione/Ricorsione.java
+++ help/help/esame/ricorsione/Ricorsione.java
@@ -1,5 +1,72 @@
 package help.esame.ricorsione;
 
 public class Ricorsione {
+	
+	public List<String> getPercorsoMassimo(String partenza){
+
+		this.ottima = new LinkedList<String>();
+
+		List<String> parziale = new LinkedList<String>();
+
+		parziale.add(partenza);
+
+	
+
+		cercaPercorso(parziale);
+
+		
+
+		return this.ottima;
+
+	}
+
+
+
+	private void cercaPercorso(List<String> parziale) {
+
+		
+
+				
+
+		//ottengo tutti i candidati
+
+		List<String> candidati = this.getSuccessori(parziale.get(parziale.size()-1));
+
+		for(String candidato : candidati) {
+
+			if(!parziale.contains(candidato)) {
+
+				//è un candidato che non ho ancora considerato
+
+				parziale.add(candidato);
+
+				this.cercaPercorso(parziale);
+
+				parziale.remove(parziale.size()-1);
+
+			}
+
+		}
+
+		
+
+		
+
+		//vedere se la soluzione corrente è migliore della ottima corrente
+
+		if(parziale.size() > ottima.size()) {
+
+			this.ottima = new LinkedList(parziale);
+
+		}
+
+		
+
+		
+
+	}
+	
+	
+	
 
 }
