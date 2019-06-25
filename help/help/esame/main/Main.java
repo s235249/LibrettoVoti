7da55383f45a675d90577592dd307cb157f75a0d
Angelica <Angelica@DESKTOP-1QUPAUU>
2019-06-25 15:36:34 +0200

    p

--- help/help/esame/main/Main.java
+++ help/help/esame/main/Main.java
@@ -1,5 +1,23 @@
 package help.esame.main;
 
-public class Main {
 
-}
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {	
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("PowerOutages.fxml"));
+			BorderPane root = (BorderPane) loader.load();
+			PowerOutagesController controller = loader.getController();
+
+			Model model = new Model();
+			controller.setModel(model);
+
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}}
